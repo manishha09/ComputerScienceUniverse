43874a6e83a42e7179215b5e841feea22ba7b966
manishha09 <manishachoudhary0998@gmail.com>
2025-07-21 15:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("codingBtn");
+
+  button.addEventListener("click", () => {
+    window.location.href = "coding-problems.html";
+  });
+});
